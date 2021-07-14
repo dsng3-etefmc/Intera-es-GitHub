51bf09fa54d03d41e5e27506ad0348597ee115cc
Gabriel F <gfp001@outlook.com>
2021-07-14 14:08:16 -0300

    right gitignore

--- Jogo/Assets/Scripts/PlayerMovement.cs
+++ Jogo/Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,10 @@ public class PlayerMovement : MonoBehaviour
 
     private bool isJumping;
     private bool doubleJump;
+    private bool isGrounded;
+
+    public int allowedJumpsOnAir = 1;
+    public int jumpsOnAir = 0;
 
     Animator animator;
 
@@ -22,7 +26,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
 
     void Start()
-    { 
+    {
         rig = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
@@ -31,55 +35,87 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        Move();
-        Jump();
-        Shoot();
+        this.CheckIfGrounded();
+
+        this.Move();
+        this.ShouldJump();
+        this.Shoot();
     }
 
-    void Shoot(){
-        if(Input.GetButtonDown("Fire1")){
-            Instantiate(ball, transform.position, transform.rotation);
-            
+    void CheckIfGrounded() {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 0.1f))
+        {
+            print("grounded");
+            Vector3 targetLocation = hit.point;
+            targetLocation += new Vector3(0, transform.localScale.y / 2, 0);
+            transform.position = targetLocation;
         }
     }
 
-    void Move(){
+    void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(ball, transform.position, transform.rotation);
+        }
+    }
+
+    void Move()
+    {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
         transform.position += movement * Time.deltaTime * Speed;
-        if(Input.GetAxis("Horizontal")>0f) {
+        if (Input.GetAxis("Horizontal") > 0f)
+        {
             animator.SetBool("run", true);
-            transform.eulerAngles = new Vector3(0f,0f,0f);
-        } 
-
-        else if (Input.GetAxis("Horizontal")<0f) {
-            animator.SetBool("run", true);
-            transform.eulerAngles = new Vector3(0f,180f,0f);
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
         }
 
-        else animator.SetBool("run", false);  
+        else if (Input.GetAxis("Horizontal") < 0f)
+        {
+            animator.SetBool("run", true);
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+
+        else animator.SetBool("run", false);
     }
 
+    void Jump(float scale)
+    {
+        rig.AddForce(new Vector2(0f, JumpForce * scale), ForceMode2D.Impulse);
+    }
 
+    void ShouldJump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
 
-    void Jump(){
-        if(Input.GetButtonDown("Jump")){
-            if(!isJumping){
-                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+            if (!isJumping)
+            {
+                this.Jump(1);
+                this.jumpsOnAir = 0;
+                print("normal jump");
             }
 
-            else if (!doubleJump){
-                rig.AddForce(new Vector2(0f, JumpForce*1.2f), ForceMode2D.Impulse);
+            else if (this.jumpsOnAir < this.allowedJumpsOnAir)
+            {
+                this.Jump(1.2f);
                 animator.SetBool("doubleJump", true);
                 doubleJump = true;
+
+                this.jumpsOnAir++;
             }
+
         }
     }
 
 
 
-    void OnCollisionEnter2D(Collision2D collision) {
+    void OnCollisionEnter2D(Collision2D collision)
+    {
 
-        if(collision.gameObject.layer == 10){
+        if (collision.gameObject.layer == 10)
+        {
             isJumping = false;
             doubleJump = false;
             animator.SetBool("jump", false);
@@ -89,9 +125,11 @@ public class PlayerMovement : MonoBehaviour
 
 
 
-    void OnCollisionExit2D(Collision2D collision) {
+    void OnCollisionExit2D(Collision2D collision)
+    {
 
-        if(collision.gameObject.layer == 10){
+        if (collision.gameObject.layer == 10)
+        {
             isJumping = true;
             animator.SetBool("jump", true);
         }
