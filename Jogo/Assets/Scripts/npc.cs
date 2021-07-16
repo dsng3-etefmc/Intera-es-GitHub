8b91b312ccf47aee5ce4e8b7adc6fc24949c00a1
Gabriel F <gfp001@outlook.com>
2021-07-16 03:15:30 -0300

    finished?

--- Jogo/Assets/Scripts/npc.cs
+++ Jogo/Assets/Scripts/npc.cs
@@ -5,26 +5,55 @@ using UnityEngine;
 public class npc : MonoBehaviour
 {
 
-    public GameObject npcDialogue;
+    public Sprite profile;
+    public string speech;
+    public string actorName = "Roberto";
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public LayerMask layer;
+
+    private DialogueController dialogue;
+
+    public float radiousInteraction;
+
+    private bool playerOnRadius;
+
+    public GameObject indicator;
+
+    public string[] sentences = {
+        "Hello, Stranger!",
+        "...",
+        "Goodbye, Stranger!"
+    };
+
+    void Start() {
+        this.dialogue = FindObjectOfType<DialogueController>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update() {
 
-    private void OnTriggerEnter2D(Collider2D collider)
-    {
-        if(collider.gameObject.layer == 15)
-        {
-            npcDialogue.SetActive(true);
-            Destroy(npcDialogue, 5f);
+        if (this.playerOnRadius) {
+            this.indicator.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.E)) {
+                dialogue.SpeechMultiple(sentences, name, profile);
+            }
+        } else {
+            this.indicator.SetActive(false);
         }
+
+    }
+
+    void FixedUpdate () {
+        this.awaitSpeech();
+    }
+
+    void awaitSpeech() {
+        Collider2D hit = Physics2D.OverlapCircle(this.transform.position, this.radiousInteraction, layer);
+
+        this.playerOnRadius = hit != null;
+    }
+
+    void OnDrawGizmosSelected() {
+        Gizmos.DrawWireSphere(transform.position, this.radiousInteraction);
     }
 }
